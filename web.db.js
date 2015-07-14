d2707f63678af301b6e6342afb88dec107b2bc77
SuperChen <cjbofemail@163.com>
2015-07-14 12:53:58 +0800

    Update web.db.js

--- web.db.js
+++ web.db.js
@@ -1,9 +1,16 @@
 var PL_db ={
   "云托管":[
-    ["sae.sina.com.cn","新浪的Sina App Engine"]
+    ["sae.sina.com.cn","新浪的云平台。"],
+    ["jae.jd.com"],"京东的云平台。"],
+    ["www.openshift.com","OpenShift是红帽的云平台。"],
+    ["www.heroku.com","Heroku是一个支持多种编程语言的云平台即服务。"],
+    ["www.appfog.com","AppFog是一个基于Cloud Foundry的PaaS平台。"],
+    ["bce.baidu.com/product/bae.html","百度的云平台。"]
   ],
-  "cdn":[
-    ["cdn.code.baidu.com","百度提供的免费CDN"]
+  "cdn公共库":[
+    ["cdn.code.baidu.com","百度提供的免费CDN资源库。"],
+    ["www.cdnjs.cn","cdnjs.cn是cdnjs在国内的镜像站点。"],
+    ["www.cdnjs.net","国内一个免费cdn资源库。"]
   ],
   "免费镜像":[
     ["ruby.taobao.org","这是一个淘宝提供的完整 rubygems.org 镜像，你可以用此代替官方版本，同步频率目前为15分钟一次以保证尽量与官方服务同步。"]
