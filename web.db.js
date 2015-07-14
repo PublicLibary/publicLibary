e28a8396350d57cc5f3eb69667252a508da39099
DemoEngine <cjbofemail@163.com>
2015-07-14 12:01:35 +0800

    Create web.db.js

---
+++ web.db.js
@@ -0,0 +1,11 @@
+var PL_db ={
+  "云托管":[
+    ["sae.sina.com.cn","新浪的Sina App Engine"]
+  ],
+  "cdn":[
+    ["cdn.code.baidu.com","百度提供的免费CDN"]
+  ],
+  "免费镜像":[
+    ["ruby.taobao.org","这是一个淘宝提供的完整 rubygems.org 镜像，你可以用此代替官方版本，同步频率目前为15分钟一次以保证尽量与官方服务同步。"]
+  ]
+};
